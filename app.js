7bf0fa93ef5e200be8d3a7d7015666e15f8de7e0
Rudolfo Borges Oliveira <rudolfoborges@Rudolfos-MacBook-Pro.local>
2014-09-15 00:34:01 -0300

    Conclusão do CRUD

--- app.js
+++ app.js
@@ -56,8 +56,10 @@ if(app.get('env') === 'development'){
     mongoose.connect('mongodb://localhost/superheros');
 }
 
+mongoose.connect('mongodb://rudolfoborges:1234@ds035270.mongolab.com:35270/superheros');
+
 if(app.get('env') === 'production'){
-    mongoose.connect('mongodb://rudolfoborges:1234@ds035270.mongolab.com:35270/superheros');
+    //mongoose.connect('mongodb://rudolfoborges:1234@ds035270.mongolab.com:35270/superheros');
 }
 
 //load all files in models dir
